8421c7251abb550954b84354e56b766f3d29348b
Vitor_Liaz88 <vnla.lic18@uea.edu.br>
2024-06-06 19:46:22 -0400

    ajuste na posicao dos componentes

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { PropertyFormComponent } from '../property-form/property-form.component';
+import { TreeComponent } from '../tree/tree.component'
 
-@Component({
-  selector: 'app-topbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './topbar.component.html',
-  styleUrl: './topbar.component.css'
-})
-export class TopbarComponent {
 
-}
+
+
+    @Component({
+      selector: 'app-topbar',
+      standalone: true,
+      imports: [PropertyFormComponent, TreeComponent],
+      templateUrl: './topbar.component.html',
+      styleUrl: './topbar.component.css'
+    })
+    export class TopbarComponent {
+
+    }
